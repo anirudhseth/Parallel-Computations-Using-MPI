7db129adf208ada7d5767b8f6f5cd831c977408e
anirudh seth <anirudhseth14@gmail.com>
2020-02-26 22:56:58 +0100

    plot

--- Assignment 2/plotOutputError.py
+++ Assignment 2/plotOutputError.py
@@ -8,10 +8,14 @@ def trueU():
 u=np.loadtxt('outputError.txt')
 
 from sklearn.metrics import mean_squared_error
+x=trueU()
 mse=[]
 for i in range(np.shape(u)[0]):
     mse.append(mean_squared_error(u[i,:], x))
-
-plt.plot(mse)
+b=np.arange(100)
+b=b*10000
+plt.plot(b,mse)
 plt.title('Mean Squared Error after every 10000 step')
+plt.xlabel('Iteration')
+plt.legend()
 plt.show()
